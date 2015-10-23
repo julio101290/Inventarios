4f61839462720fda2a0d5cebb1972582c15f62b6
julio <julio@julio-Lenovo-G480>
2015-10-22 23:42:43 -0600

    Correcciones

---
+++ Inventarios/BaseDatos/INVEN.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 23-10-2015 a las 07:36:22
+-- Versión del servidor: 5.6.25
+-- Versión de PHP: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `INVEN`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_InsertaCliente`(in  Nombres VARCHAR(200)
+, Apellidos VARCHAR(200),Direccion varchar(200),Ciudad varchar(200)
+,Telefono varchar(200),RFC VARCHAR(10),FechaNacimiento VARCHAR(200),Estado VARCHAR(200)
+,Municipio varchar(200),CodigoPostal VARCHAR(200))
+BEGIN
+ 
+    INSERT INTO `Clientes`
+    (
+    `Nombres`,
+    `Apellidos`,
+    `Direccion`,
+    `Ciudad`,
+    `Telefono`,
+    `RFC`,
+    `FechaNacimiento`,
+    `Estado`,
+    `Municipio`,
+    `CodigoPostal`
+        )
+    VALUES
+    (
+    Nombres,
+    Apellidos,
+    Direccion,
+    ciudad,
+    Telefono,
+    RFC,
+    FechaNacimiento,
+    Estado,
+    Municipio,
+    CodigoPostal
+    );
+ 
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `Clientes` (
+  `idCliente` bigint(20) NOT NULL,
+  `Nombres` varchar(100) DEFAULT NULL,
+  `Apellidos` varchar(45) DEFAULT NULL,
+  `Direccion` varchar(45) DEFAULT NULL,
+  `Ciudad` varchar(45) DEFAULT NULL,
+  `Telefono` varchar(45) DEFAULT NULL,
+  `RFC` varchar(45) DEFAULT NULL,
+  `FechaNacimiento` datetime(6) DEFAULT NULL,
+  `Estado` varchar(45) DEFAULT NULL,
+  `Municipio` varchar(45) DEFAULT NULL,
+  `CodigoPostal` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `Usuarios` (
+  `idUsuario` bigint(20) unsigned NOT NULL,
+  `Usuario` varchar(300) NOT NULL,
+  `Contra` varchar(300) NOT NULL,
+  `Grupo` mediumint(9) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `Usuarios`
+--
+
+INSERT INTO `Usuarios` (`idUsuario`, `Usuario`, `Contra`, `Grupo`) VALUES
+(1, 'Julio', 'Cesar', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `Usuario` (`Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  MODIFY `idUsuario` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
