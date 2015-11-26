d435fcd79af3513eb2684d8eb1e575b1330b59f5
julio <julio@julio-Lenovo-G480>
2015-11-26 00:20:42 -0700

    Cambios en el diseño

--- Inventarios/src/interfaces/frmPrincipal.java
+++ Inventarios/src/interfaces/frmPrincipal.java
@@ -37,42 +37,66 @@ control_existencias ctrl = new control_existencias();
     private void initComponents() {
 
         jDesktopPane1 = new javax.swing.JDesktopPane();
+        jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu6 = new javax.swing.JMenu();
-        jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
-        Buscararticulos = new javax.swing.JMenuItem();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
         jDesktopPane1.setBackground(java.awt.Color.lightGray);
 
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Los Mochis.jpg"))); // NOI18N
+        jLabel1.setLabelFor(jDesktopPane1);
+        jDesktopPane1.add(jLabel1);
+        jLabel1.setBounds(0, 0, 1480, 610);
+
+        jMenu6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/server-database.png"))); // NOI18N
         jMenu6.setText("Configuración         ");
-        jMenu6.setFont(new java.awt.Font("Segoe UI Semibold", 0, 24)); // NOI18N
-        jMenuBar1.add(jMenu6);
+        jMenu6.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jMenu6.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+        jMenu6.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
 
-        jMenu1.setBorder(new javax.swing.border.MatteBorder(null));
-        jMenu1.setText("Clientes");
-        jMenu1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 24)); // NOI18N
-        jMenu1.setPreferredSize(new java.awt.Dimension(200, 50));
+        jMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_G, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem2.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jMenuItem2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/gente.jpeg"))); // NOI18N
+        jMenuItem2.setText("Grupos usuarios");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu6.add(jMenuItem2);
 
-        jMenuItem1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jMenuItem1.setText("Buscar clientes");
-        jMenuItem1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_U, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/userr.png"))); // NOI18N
+        jMenuItem1.setText("Usuarios");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem1ActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem1);
+        jMenu6.add(jMenuItem1);
 
+        jMenuBar1.add(jMenu6);
+
+        jMenu1.setBorder(new javax.swing.border.MatteBorder(null));
+        jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ALUMNOS2.JPG"))); // NOI18N
+        jMenu1.setText("Clientes");
+        jMenu1.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        jMenu1.setPreferredSize(new java.awt.Dimension(200, 50));
+
+        jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
         jMenuItem4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jMenuItem4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/bookmark-new.png"))); // NOI18N
         jMenuItem4.setText("Registrar");
         jMenuItem4.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
@@ -85,11 +109,14 @@ control_existencias ctrl = new control_existencias();
         jMenuBar1.add(jMenu1);
 
         jMenu2.setBorder(new javax.swing.border.MatteBorder(null));
+        jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/índice.jpeg"))); // NOI18N
         jMenu2.setText("Artículos");
-        jMenu2.setFont(new java.awt.Font("Segoe UI Semibold", 0, 24)); // NOI18N
+        jMenu2.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
         jMenu2.setPreferredSize(new java.awt.Dimension(200, 50));
 
+        jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
         jMenuItem3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jMenuItem3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/bookmark-new.png"))); // NOI18N
         jMenuItem3.setText("Registar");
         jMenuItem3.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
@@ -99,36 +126,6 @@ control_existencias ctrl = new control_existencias();
         });
         jMenu2.add(jMenuItem3);
 
-        Buscararticulos.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        Buscararticulos.setText("Lista de articulos");
-        Buscararticulos.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        Buscararticulos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BuscararticulosActionPerformed(evt);
-            }
-        });
-        jMenu2.add(Buscararticulos);
-
-        jMenuItem6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jMenuItem6.setText("Actualizar stock");
-        jMenuItem6.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem6ActionPerformed(evt);
-            }
-        });
-        jMenu2.add(jMenuItem6);
-
-        jMenuItem8.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jMenuItem8.setText("Devoluciones");
-        jMenuItem8.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jMenuItem8.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem8ActionPerformed(evt);
-            }
-        });
-        jMenu2.add(jMenuItem8);
-
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -137,23 +134,20 @@ control_existencias ctrl = new control_existencias();
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1500, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jDesktopPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1476, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jDesktopPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 613, Short.MAX_VALUE)
+                .addComponent(jDesktopPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 610, Short.MAX_VALUE)
                 .addGap(24, 24, 24))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-       frmBuscarclientes buscar = new frmBuscarclientes();
-        jDesktopPane1.add(buscar);
-        buscar.show(); 
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
        
@@ -174,36 +168,27 @@ control_existencias ctrl = new control_existencias();
     
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
-    private void BuscararticulosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BuscararticulosActionPerformed
-        frmBuscararticulos bus = new frmBuscararticulos();
-        jDesktopPane1.add(bus);
-        bus.show(); 
-    }//GEN-LAST:event_BuscararticulosActionPerformed
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        frmGruposUsuarios grupo = new frmGruposUsuarios();
+        jDesktopPane1.add(grupo);
+        grupo.setVisible(true);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
-    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
-//    frmActualizarstock stock = new frmActualizarstock();
-//    jDesktopPane1.add(stock);
-//    stock.show();
-    }//GEN-LAST:event_jMenuItem6ActionPerformed
-
-    private void jMenuItem8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem8ActionPerformed
-    frmDevoluciones dev = new frmDevoluciones();
-    jDesktopPane1.add(dev);
-    dev.show();
-    }//GEN-LAST:event_jMenuItem8ActionPerformed
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenuItem Buscararticulos;
     private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu6;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem8;
     // End of variables declaration//GEN-END:variables
 }
